79f3f70643ec0802c6cb53e9d90056ce249ef88f
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2016-05-13 13:58:23 +0100

    spermarkets script v1

---
+++ Assets/Scripts/Supermarkets/SupermarketDemand.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SupermarketDemand : MonoBehaviour {
+
+    [HideInInspector] public int pears;
+    [HideInInspector] public int strawberries;
+    [HideInInspector] public int apples;
+    [HideInInspector] public int bananas;
+
+    void Start () {
+        pears = Random.Range(0, 20);
+        strawberries = Random.Range(0, 20);
+        apples = Random.Range(0, 20);
+        bananas = Random.Range(0, 20);
+    }
+}
